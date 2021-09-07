6178b9835ca9d06db76beba34a19858e7042e8be
shuvo-sutradhar <shuvossd1@gmail.com>
2021-09-07 13:50:35 +0600

    commit 4

--- effect.js
+++ effect.js
@@ -6,7 +6,7 @@ $('document').ready(function(){
 		var vw;
 		$(window).resize(function(){
 			 vw = $(window).width()/2;
-			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7,#b8,#b9, #b10, #b11').stop();
+			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7,#b8,#b9, #b10, #b00').stop();
 			$('#b11').animate({top:240, left: vw-350},500);
 			$('#b22').animate({top:240, left: vw-250},500);
 			$('#b33').animate({top:240, left: vw-150},500);
@@ -131,7 +131,7 @@ $('document').ready(function(){
 	function loop11() {
 		var randleft = 1000*Math.random();
 		var randtop = 500*Math.random();
-		$('#b11').animate({left:randleft,bottom:randtop},10000,function(){
+		$('#b00').animate({left:randleft,bottom:randtop},10000,function(){
 			loop11();
 		});
 	}
@@ -139,7 +139,7 @@ $('document').ready(function(){
 	$('#balloons_flying').click(function(){
 		$('.balloon-border').animate({top:-500},8000);
 		$('#b1,#b4,#b5,#b7, #b9,#b10').addClass('balloons-rotate-behaviour-one');
-		$('#b2,#b3,#b6, #b8,#b11').addClass('balloons-rotate-behaviour-two');
+		$('#b2,#b3,#b6, #b8,#b00').addClass('balloons-rotate-behaviour-two');
 		// $('#b3').addClass('balloons-rotate-behaviour-two');
 		// $('#b4').addClass('balloons-rotate-behaviour-one');
 		// $('#b5').addClass('balloons-rotate-behaviour-one');
@@ -182,7 +182,7 @@ $('document').ready(function(){
 	$('#wish_message').click(function(){
 		 vw = $(window).width()/2;
 
-		$('#b1,#b2,#b3,#b4,#b5,#b6,#b7,#b8, #b9, #b10, #b00').stop();
+		$('#b1,#b2,#b3,#b4,#b5,#b6,#b7,#b8, #b9, #b10, #b0').stop();
 		$('#b1').attr('id','b11');
 		$('#b2').attr('id','b22')
 		$('#b3').attr('id','b33')
@@ -205,7 +205,7 @@ $('document').ready(function(){
 		$('#b88').animate({top:300, left: vw+0},500);
 		$('#b99').animate({top:300, left: vw+50},500);
 		$('#b100').animate({top:300, left: vw+100},500);
-		$('#b00').animate({top:300, left: vw+150},500);
+		$('#b00').animate({top:300, left: vw+130},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
